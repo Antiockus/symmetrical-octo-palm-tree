da04a52d3792c8577353345cd360522c6f71bd1a
Tim Squires <antiockus@gmail.com>
2020-01-07 11:39:00 -0500

    todo class

--- src/todo.php
+++ src/todo.php
@@ -1,7 +1,8 @@
 <?php
+    declare(strict_types=1);
 namespace Antiockus;
 
-class todo
+final class todo
 {
     public $todo;
 
---
+++ tests/todoTest.php
@@ -0,0 +1,9 @@
+<?php
+
+    namespace Antiockus;
+
+
+    class todoTest extends \PHPUnit_Framework_TestCase
+    {
+
+    }
